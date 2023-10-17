035050376c280edb0dd8b58ef9792d52d64863b5
TIFFANY MAINA <tiffmaina@gmail.com>
2023-10-17 21:48:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+// const newHead = document.querySelector("body"); 
+const newHeader = document.createElement("h1");
+// newHeader.append(header);
+newHeader.id = 'victory';
+newHeader.textContent = `Tiffany is the champion`;
+
+// const newHeader = document.querySelector("#victory");
+// console.log('this');
+// const heading = document.getElementById("victory");
+
+// // const newHead = document.getElementByTagName("h1");
+// const newHeader = document.querySelector('#victory');
+
+
+// const ul = document.createElement("ul");
+//  for (let i = o; i < 3; i++){
+//     const li = document.createElement("li");
+//     li.textContent = (i+1).toString();
+//     ul.append(li);
+//  }
+
+// element.append(ul);
